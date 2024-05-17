14d5d3d2f664f43ae7816502a3731e5c59ed427b
PedroQuessada <pedro.rafael.quessada@gmail.com>
2024-05-17 16:36:46 -0300

    create test for usecase

---
+++ src/internal/application/useCases/payment/approve-payment.usecase.spec.ts
@@ -0,0 +1,70 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { NotFoundException, Inject } from '@nestjs/common';
+import { ApprovePaymentByOrderId } from './approve-payment.usecase';
+import { IPaymentRepository } from 'src/internal/domain/payment/repositories/payment.repository';
+import { ChangedPaymentStatusEvent } from 'src/internal/domain/payment/events/payment-status-changed.event';
+import { IEventEmitter } from '../../ports/events/event';
+import { Payment } from 'src/internal/domain/payment/entities/payment.entity';
+
+describe('ApprovePaymentByOrderId', () => {
+  let approvePaymentByOrderId: ApprovePaymentByOrderId;
+  let paymentRepositoryMock: jest.Mocked<IPaymentRepository>;
+  let eventEmitterMock: jest.Mocked<IEventEmitter>;
+
+  beforeEach(async () => {
+    jest.useFakeTimers();
+    paymentRepositoryMock = {
+      findOneByOrderId: jest.fn(),
+    } as unknown as jest.Mocked<IPaymentRepository>;
+
+    eventEmitterMock = {
+      emit: jest.fn(),
+    } as unknown as jest.Mocked<IEventEmitter>;
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ApprovePaymentByOrderId,
+        { provide: 'PaymentRepository', useValue: paymentRepositoryMock },
+        { provide: 'EventEmitter', useValue: eventEmitterMock },
+      ],
+    }).compile();
+
+    approvePaymentByOrderId = module.get<ApprovePaymentByOrderId>(ApprovePaymentByOrderId);
+  });
+
+  afterEach(() => {
+    jest.useRealTimers(); // Restore real timers after each test
+  });
+
+  describe('execute', () => {
+    it('should approve the payment and emit events', async () => {
+      const orderId = 'testOrderId';
+      const customerId = 'testCustomerId';
+      const payment = new Payment({
+        id: 'paymentId1',
+        customerId: customerId,
+        orderId: orderId,
+        value: 10,
+      });
+      payment.status = 'Pendente de pagamento';
+
+      paymentRepositoryMock.findOneByOrderId.mockResolvedValue(payment);
+      
+      await approvePaymentByOrderId.execute(orderId);
+      jest.advanceTimersByTime(20000);
+
+      expect(eventEmitterMock.emit).toHaveBeenCalledWith(
+        'payment-status.changed',
+        expect.any(ChangedPaymentStatusEvent),
+      );
+    });
+
+    it('should throw NotFoundException if the payment does not exist', async () => {
+      const orderId = 'nonExistentOrderId';
+
+      paymentRepositoryMock.findOneByOrderId.mockResolvedValue(null);
+
+      await expect(approvePaymentByOrderId.execute(orderId)).rejects.toThrow(NotFoundException);
+    });
+  });
+});
---
+++ src/internal/application/useCases/payment/find-one-payment-by-order.usecase.spec.ts
@@ -0,0 +1,52 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { NotFoundException, Inject } from '@nestjs/common';
+import { FindOnePaymentByOrderId } from './find-one-payment-by-order.usecase';
+import { IPaymentRepository } from 'src/internal/domain/payment/repositories/payment.repository';
+import { Payment } from 'src/internal/domain/payment/entities/payment.entity';
+
+describe('FindOnePaymentByOrderId', () => {
+  let findOnePaymentByOrderId: FindOnePaymentByOrderId;
+  let paymentRepositoryMock: jest.Mocked<IPaymentRepository>;
+
+  beforeEach(async () => {
+    paymentRepositoryMock = {
+      findOneByOrderId: jest.fn(),
+    } as unknown as jest.Mocked<IPaymentRepository>;
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        FindOnePaymentByOrderId,
+        { provide: 'PaymentRepository', useValue: paymentRepositoryMock },
+      ],
+    }).compile();
+
+    findOnePaymentByOrderId = module.get<FindOnePaymentByOrderId>(FindOnePaymentByOrderId);
+  });
+
+  describe('execute', () => {
+    it('should find and return the payment', async () => {
+      const orderId = 'testOrderId';
+      const customerId = 'testCustomerId';
+      const payment = new Payment({
+        id: 'paymentId1',
+        customerId: customerId,
+        orderId: orderId,
+        value: 10,
+      });
+
+      paymentRepositoryMock.findOneByOrderId.mockResolvedValue(payment);
+
+      const result = await findOnePaymentByOrderId.execute(orderId);
+
+      expect(result).toEqual(payment);
+    });
+
+    it('should throw NotFoundException if the payment does not exist', async () => {
+      const orderId = 'nonExistentOrderId';
+
+      paymentRepositoryMock.findOneByOrderId.mockResolvedValue(null);
+
+      await expect(findOnePaymentByOrderId.execute(orderId)).rejects.toThrow(NotFoundException);
+    });
+  });
+});
