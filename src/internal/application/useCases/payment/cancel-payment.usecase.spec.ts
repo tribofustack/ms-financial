e62f826dc514a3e494d53f5d939250d3aa52bb3e
PedroQuessada <pedro.rafael.quessada@gmail.com>
2024-05-17 22:32:20 -0300

    create test

---
+++ src/internal/application/useCases/payment/cancel-payment.usecase.spec.ts
@@ -0,0 +1,85 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { NotFoundException } from '@nestjs/common';
+import { DomainException } from 'src/internal/application/errors';
+import { CancelPaymentByOrderId } from './cancel-payment.usecase';
+import { IPaymentRepository } from 'src/internal/domain/payment/repositories/payment.repository';
+import { ChangedPaymentStatusEvent } from 'src/internal/domain/payment/events/payment-status-changed.event';
+import { IEventEmitter } from '../../ports/events/event';
+import { Payment } from 'src/internal/domain/payment/entities/payment.entity';
+
+describe('CancelPaymentByOrderId', () => {
+  let cancelPaymentByOrderId: CancelPaymentByOrderId;
+  let paymentRepositoryMock: jest.Mocked<IPaymentRepository>;
+  let eventEmitterMock: jest.Mocked<IEventEmitter>;
+
+  beforeAll(async () => {
+    jest.useFakeTimers();
+    paymentRepositoryMock = {
+      findOneByOrderId: jest.fn(),
+    } as unknown as jest.Mocked<IPaymentRepository>;
+
+    eventEmitterMock = {
+      emit: jest.fn(),
+    } as unknown as jest.Mocked<IEventEmitter>;
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CancelPaymentByOrderId,
+        { provide: 'PaymentRepository', useValue: paymentRepositoryMock },
+        { provide: 'EventEmitter', useValue: eventEmitterMock },
+      ],
+    }).compile();
+
+    cancelPaymentByOrderId = module.get<CancelPaymentByOrderId>(CancelPaymentByOrderId);
+  });
+
+  afterEach(() => {
+    jest.useRealTimers(); // Restore real timers after each test
+  });
+
+  describe('execute', () => {
+    it('should cancel the payment and emit events', async () => {
+      const orderId = 'testOrderId';
+      const payment = new Payment({
+        id: 'paymentId1',
+        customerId: 'testCustomerId',
+        orderId: orderId,
+        value: 10,
+      });
+      payment.status = 'Pendente de pagamento';
+
+      paymentRepositoryMock.findOneByOrderId.mockResolvedValue(payment);
+      
+      await cancelPaymentByOrderId.execute(orderId);
+      jest.advanceTimersByTime(20000);
+
+      expect(eventEmitterMock.emit).toHaveBeenCalledWith(
+        'payment-status.changed',
+        expect.any(ChangedPaymentStatusEvent),
+      );
+    });
+
+    it('should throw DomainException if the payment was already approved', async () => {
+      const orderId = 'testOrderId';
+      const payment = new Payment({
+        id: 'paymentId1',
+        customerId: 'testCustomerId',
+        orderId: orderId,
+        value: 10,
+      });
+      payment.status = 'Aprovado';
+
+      paymentRepositoryMock.findOneByOrderId.mockResolvedValue(payment);
+
+      await expect(cancelPaymentByOrderId.execute(orderId)).rejects.toThrow(DomainException);
+    });
+
+    it('should throw NotFoundException if the payment does not exist', async () => {
+      const orderId = 'nonExistentOrderId';
+
+      paymentRepositoryMock.findOneByOrderId.mockResolvedValue(null);
+
+      await expect(cancelPaymentByOrderId.execute(orderId)).rejects.toThrow(NotFoundException);
+    });
+  });
+});
---
+++ src/internal/application/useCases/payment/create-payment.usecase.spec.ts
@@ -0,0 +1,104 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { DomainException } from 'src/internal/application/errors';
+import { CreatePayment } from './create-payment.usecase';
+import { Payment } from 'src/internal/domain/payment/entities/payment.entity';
+import { IPaymentRepository } from 'src/internal/domain/payment/repositories/payment.repository';
+import { IPaymentIntegration } from 'src/internal/application/ports/integrations/payment';
+import { IIdentifierGenerator } from 'src/internal/application/ports/tokens/id-generator';
+import { CreatedPaymentEvent } from 'src/internal/domain/payment/events/payment-created.event';
+import { IEventEmitter } from '../../ports/events/event';
+
+describe('CreatePayment', () => {
+  let createPayment: CreatePayment;
+  let paymentRepositoryMock: jest.Mocked<IPaymentRepository>;
+  let paymentIntegrationMock: jest.Mocked<IPaymentIntegration>;
+  let idGeneratorMock: jest.Mocked<IIdentifierGenerator>;
+  let eventEmitterMock: jest.Mocked<IEventEmitter>;
+
+  beforeAll(async () => {
+    paymentRepositoryMock = {
+      create: jest.fn(),
+    } as unknown as jest.Mocked<IPaymentRepository>;
+
+    paymentIntegrationMock = {
+      createPayment: jest.fn(),
+    } as unknown as jest.Mocked<IPaymentIntegration>;
+
+    idGeneratorMock = {
+      generate: jest.fn().mockReturnValue('paymentId1'),
+    } as unknown as jest.Mocked<IIdentifierGenerator>;
+
+    eventEmitterMock = {
+      emit: jest.fn(),
+    } as unknown as jest.Mocked<IEventEmitter>;
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CreatePayment,
+        { provide: 'PaymentRepository', useValue: paymentRepositoryMock },
+        { provide: 'PaymentIntegration', useValue: paymentIntegrationMock },
+        { provide: 'IdGenerator', useValue: idGeneratorMock },
+        { provide: 'EventEmitter', useValue: eventEmitterMock },
+      ],
+    }).compile();
+
+    createPayment = module.get<CreatePayment>(CreatePayment);
+  });
+
+  describe('execute', () => {
+    it('should create a payment successfully', async () => {
+      const order = {
+        id: 'orderId1',
+        customerId: 'customerId1',
+        total: 10,
+      };
+
+      const mockQrCode = 'mockQrCode';
+      const mockUrl = 'mockUrl';
+      const mockStatus = 'pending';
+
+      paymentIntegrationMock.createPayment.mockResolvedValue({
+        id: 'paymentId1',
+        qrCode: mockQrCode,
+        url: mockUrl,
+        status: mockStatus,
+      });
+
+      await createPayment.execute(order);
+
+      expect(paymentRepositoryMock.create).toHaveBeenCalledWith(
+        expect.objectContaining({
+          id: 'paymentId1',
+          customerId: 'customerId1',
+          orderId: 'orderId1',
+          value: 10,
+          status: 'Pendente de pagamento',
+        }),
+      );
+
+      expect(eventEmitterMock.emit).toHaveBeenCalledWith(
+        'payment.created',
+        expect.any(CreatedPaymentEvent),
+      );
+    });
+
+    it('should throw a DomainException if payment creation fails', async () => {
+      const order = {
+        id: 'orderId1',
+        customerId: 'customerId1',
+        total: 10,
+      };
+
+      const mockStatus = 'cancelled';
+
+      paymentIntegrationMock.createPayment.mockResolvedValue({
+        id: 'paymentId1',
+        qrCode: 'mockQrCode',
+        url: 'mockUrl',
+        status: mockStatus,
+      });
+
+      await expect(createPayment.execute(order)).rejects.toThrow(DomainException);
+    });
+  });
+});
