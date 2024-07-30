58764af1fbfaf115e8a23beb51f645cd259bffc5
deal-machine <95deal@gmail.com>
2024-07-30 14:28:20 -0300

    fix: tests

--- src/internal/application/useCases/payment/approve-payment.usecase.spec.ts
+++ src/internal/application/useCases/payment/approve-payment.usecase.spec.ts
@@ -1,12 +1,12 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { NotFoundException, Inject } from '@nestjs/common';
-import { ApprovePaymentByOrderId } from './approve-payment.usecase';
-import { IPaymentRepository } from 'src/internal/domain/payment/repositories/payment.repository';
-import { ChangedPaymentStatusEvent } from 'src/internal/domain/payment/events/payment-status-changed.event';
-import { IEventEmitter } from '../../ports/events/event';
-import { Payment } from 'src/internal/domain/payment/entities/payment.entity';
+import { Test, TestingModule } from "@nestjs/testing";
+import { NotFoundException } from "@nestjs/common";
+import { ApprovePaymentByOrderId } from "./approve-payment.usecase";
+import { IPaymentRepository } from "src/internal/domain/payment/repositories/payment.repository";
+import { ChangedPaymentStatusEvent } from "src/internal/domain/payment/events/payment-status-changed.event";
+import { IEventEmitter } from "../../ports/events/event";
+import { Payment } from "src/internal/domain/payment/entities/payment.entity";
 
-describe('ApprovePaymentByOrderId', () => {
+describe("ApprovePaymentByOrderId", () => {
   let approvePaymentByOrderId: ApprovePaymentByOrderId;
   let paymentRepositoryMock: jest.Mocked<IPaymentRepository>;
   let eventEmitterMock: jest.Mocked<IEventEmitter>;
@@ -15,6 +15,7 @@ describe('ApprovePaymentByOrderId', () => {
     jest.useFakeTimers();
     paymentRepositoryMock = {
       findOneByOrderId: jest.fn(),
+      changeStatus: jest.fn(),
     } as unknown as jest.Mocked<IPaymentRepository>;
 
     eventEmitterMock = {
@@ -24,47 +25,51 @@ describe('ApprovePaymentByOrderId', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         ApprovePaymentByOrderId,
-        { provide: 'PaymentRepository', useValue: paymentRepositoryMock },
-        { provide: 'EventEmitter', useValue: eventEmitterMock },
+        { provide: "PaymentRepository", useValue: paymentRepositoryMock },
+        { provide: "EventEmitter", useValue: eventEmitterMock },
       ],
     }).compile();
 
-    approvePaymentByOrderId = module.get<ApprovePaymentByOrderId>(ApprovePaymentByOrderId);
+    approvePaymentByOrderId = module.get<ApprovePaymentByOrderId>(
+      ApprovePaymentByOrderId,
+    );
   });
 
   afterEach(() => {
     jest.useRealTimers(); // Restore real timers after each test
   });
 
-  describe('execute', () => {
-    it('should approve the payment and emit events', async () => {
-      const orderId = 'testOrderId';
-      const customerId = 'testCustomerId';
+  describe("execute", () => {
+    it("should approve the payment and emit events", async () => {
+      const orderId = "testOrderId";
+      const customerId = "testCustomerId";
       const payment = new Payment({
-        id: 'paymentId1',
+        id: "paymentId1",
         customerId: customerId,
         orderId: orderId,
         value: 10,
       });
-      payment.status = 'Pendente de pagamento';
+      payment.status = "Pendente de pagamento";
 
       paymentRepositoryMock.findOneByOrderId.mockResolvedValue(payment);
-      
+
       await approvePaymentByOrderId.execute(orderId);
       jest.advanceTimersByTime(20000);
 
       expect(eventEmitterMock.emit).toHaveBeenCalledWith(
-        'payment-status.changed',
+        "payment-status.changed",
         expect.any(ChangedPaymentStatusEvent),
       );
     });
 
-    it('should throw NotFoundException if the payment does not exist', async () => {
-      const orderId = 'nonExistentOrderId';
+    it("should throw NotFoundException if the payment does not exist", async () => {
+      const orderId = "nonExistentOrderId";
 
       paymentRepositoryMock.findOneByOrderId.mockResolvedValue(null);
 
-      await expect(approvePaymentByOrderId.execute(orderId)).rejects.toThrow(NotFoundException);
+      await expect(approvePaymentByOrderId.execute(orderId)).rejects.toThrow(
+        NotFoundException,
+      );
     });
   });
 });
